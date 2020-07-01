1711cc1efa2bb3d1770b3724f242dc73daf066cd
Sillas Vidal <svidalempresa@gmail.com>
2020-07-01 16:54:37 -0300

    Configurando conexão com o banco de dados

---
+++ src/config/database.js
@@ -0,0 +1,24 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(
+    'authentication',
+    'SA',
+    'S35SUP5RSRG',
+    {
+        host: 'localhost',
+        dialect: 'mssql'
+    }
+);
+
+async function teste() {
+    try{
+        await sequelize.authenticate();
+        console.log('Conexão com o banco de dados realizada com sucesso!');
+    }catch{
+        console.log('Não foi possível se conectar ao banco de dados.');
+    }
+}
+
+teste();
+
+module.exports = sequelize;
